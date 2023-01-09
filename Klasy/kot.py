7836aa2bb897ce6c5f63e17eb07e4debfc235c8a
Gutek <115694908+Gutek132@users.noreply.github.com>
2023-01-09 22:20:06 +0100

    Add files via upload

---
+++ Klasy/kot.py
@@ -0,0 +1,23 @@
+class Kot():
+    masa_ciala = 0
+    wiek = 0
+    imie = ""
+    plec = True
+    kolor_siersci = ""
+    kolor_oczu = ""
+    dlugosc_ogona = 0
+    wykastrowanie = True
+    puszystosc = "" 
+    rodzaj_siersci = ""
+
+    def inf(self):
+        print(self.masa_ciala)
+        print(self.wiek)
+        print(self.imie)
+        print(self.plec)
+        print(self.kolor_siersci)
+        print(self.kolor_oczu)
+        print(self.dlugosc_ogona)
+        print(self.wykastrowanie)
+        print(self.puszystosc)
+        print(self.rodzaj_siersci)
---
+++ Klasy/zadaniamain.py
@@ -0,0 +1,6 @@
+from kot import Kot
+
+p1 = Kot()
+
+
+p1.inf()
